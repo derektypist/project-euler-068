2d2ff8980ecd923ad61e9113693f392e66261f33
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-12-23 15:51:14 +0000

    Added showSolution function at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Function to Show Solution
+function showSolution() {
+    // Set Up Variable
+    let txt = `Solution is ${magic5GonRing()} <br>`;
+    // Display Information in the Browser
+    document.getElementById("solutioninfo").innerHTML = txt;
+}
